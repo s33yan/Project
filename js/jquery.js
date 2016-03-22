81d9053c6f1de63345758cd8040cf988bc6d4cb9
Sara Yan <sara.yan308@gmail.com>
2016-03-23 00:44:49 +0800

    Initial commit

---
+++ js/jquery.js
@@ -0,0 +1,18 @@
+//code from link below--
+//$(window).scroll(
+//    {
+//        previousTop: 0
+//    }, 
+//    function () {
+//    var currentTop = $(window).scrollTop();
+//    if (currentTop < this.previousTop) {
+//        $(".sidebar em").text("Up"); /* optional for demo */
+//        $(".header").show();
+//    } else {
+//        $(".sidebar em").text("Down");
+//        $(".header").hide();
+//    }
+//    this.previousTop = currentTop;
+//});
+
+//the web link--http://stackoverflow.com/questions/17136513/make-a-fixed-nav-bar-disappear-when-the-user-scrolls-down-and-reappear-upon-scro
